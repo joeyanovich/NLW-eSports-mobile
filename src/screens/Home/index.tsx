3eb90968385c56322e5870dc084ff8231fcd0d0a
joeyanovich <joedison.dias@gmail.com>
2022-10-15 02:56:42 -0300

    Added the other games and their information (title and subtitle)

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { View, Image } from 'react-native';
+import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png'
 import { GameCard } from '../../components/GameCard';
@@ -21,9 +21,19 @@ export function Home() {
         subtitle="Selecione o jogo que deseja jogar..."
       />
 
-      <GameCard
-      data={GAMES[0]}
+      <FlatList
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <GameCard
+          data={item}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
       />
+      
     </View>
   );
 }
